c42936fe8d9fe1e8bef56dc37634b0fc9c460188
Dan Haugh <daniel_haugh@brown.edu>
2015-03-07 17:07:08 +0000

    rewrote getGpioFromPin in C for performance

---
+++ fasterGetGpioFromPin.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main(int argc, char* argv[]){
+	int pinNumber = atoi(argv[1]);
+	switch(pinNumber){
+		case 0:
+			printf("11");
+			break;
+		case 1:
+			printf("12");
+			break;
+		case 2:
+			printf("13");
+			break;
+		case 3:
+			printf("14");
+			break;
+		case 4:
+			printf("6");
+			break;
+		case 5:
+			printf("0");
+			break;
+		case 6:
+			printf("1");
+			break;
+		case 7:
+			printf("38");
+			break;
+		case 8:
+			printf("40");
+			break;
+		case 9:
+			printf("4");
+			break;
+		case 10:
+			printf("10");
+			break;
+		case 11:
+			printf("5");
+			break;
+		case 12:
+			printf("15");
+			break;
+		case 13:
+			printf("7");
+			break;
+
+	}
+
+}
+
